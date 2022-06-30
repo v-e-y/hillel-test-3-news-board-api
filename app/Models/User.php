944ed37f27e7727bd35d29a3e3cb61c4b1b1e8c9
V-E-Y <v.eduard.y@gmail.com>
2022-06-30 22:58:56 +0300

    new branch dev

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,11 +10,12 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens; 
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
-     *
      * @var array<int, string>
      */
     protected $fillable = [
@@ -25,7 +26,6 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for serialization.
-     *
      * @var array<int, string>
      */
     protected $hidden = [
@@ -35,7 +35,6 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be cast.
-     *
      * @var array<string, string>
      */
     protected $casts = [
